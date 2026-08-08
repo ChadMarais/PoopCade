0e7d05760505d671127f7cbaabfbff737dc6ea72
ChadMaraisLaptop <Chad@GeminiSolution.co.za>
2026-08-08 20:46:21 +0200

    Tighten Poopcade responsive menu screen

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'poopcade-';
-const CACHE_NAME = `${CACHE_PREFIX}shell-v8`;
+const CACHE_NAME = `${CACHE_PREFIX}shell-v9`;
 const SUPABASE_ORIGIN = 'https://kpssybcwwmtcdhrmfcgc.supabase.co';
 
 // These routes must exist for the offline shell to install successfully.
